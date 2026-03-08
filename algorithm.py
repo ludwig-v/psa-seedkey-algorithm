89eb2d3b4f8d65059fbe8e60e66d66c389e78f7d
Ludwig <5264048+ludwig-v@users.noreply.github.com>
2026-03-08 12:32:28 +0100

    Add algorithm.py (Python port)

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,4 @@
 """
-Copyright 2021, Ludwig V. <https://github.com/ludwig-v>
 Original algorithm by Wouter Bokslag & Jason F. <https://github.com/prototux>
 
 This program is free software: you can redistribute it and/or modify
